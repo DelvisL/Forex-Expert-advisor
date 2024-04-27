2d2c677b5b931b7105381242b416883b84607eec
Delvis Langat <80012247+DelvisL@users.noreply.github.com>
2024-04-26 22:46:08 -0400

    Update SL_TP_EA.py

--- SL_TP_EA.py
+++ SL_TP_EA.py
@@ -1,8 +1,7 @@
 import socket
 import MetaTrader5 as mt5
-import re,time,pdb,time,threading,sys,os,traceback
+import re,time,pdb,time,threading,sys,os,traceback,subprocess
 import win32service,servicemanager,win32serviceutil,win32evtlog
-
 
 
 class TradingService(win32serviceutil.ServiceFramework):
@@ -39,6 +38,7 @@ class TradingService(win32serviceutil.ServiceFramework):
             try:
                 main2()
             except:
+                
                 servicemanager.LogErrorMsg(traceback.format_exc())
         
 
@@ -54,13 +54,69 @@ if __name__=='__main__':
 
 #starts the process of calling necessary functions
 def main2():
-  login=MT5 username
-  password=MT5 password
-  server=MT5 server
-  terminalconnect(login,password,server)
+  global login
+  login=22334134
+  global password
+  password="5(jt0MqVowo!"
+  global server
+  server="Forex.com-Demo 535"
+  checkservice()
+  #reconnectThread=threading.Thread(target=checkservice,args=())
+  #reconnectThread.start()
   createsocket()                                                                                    
 
-
+def openpositions():
+    openTrades=mt5.positions_get()
+    for  i in openTrades:
+        position=i.type
+        if position==1:position="false"
+        elif position==0:position="true"
+        dparsed=i.price_current
+        ticket=i.ticket
+        ATR=(i.price_open-i.sl)/1.5
+        pair=i.symbol
+        servicemanager.LogInfoMsg(str(i))
+        tp=0.000
+        stoploss=dparsed-(ATR*1.5)
+        opThread=threading.Thread(target=limits,args=(dparsed,ATR,ticket,pair,position,))
+        opThread.start()
+        time.sleep(5)
+        
+def services():
+    try:
+        _=subprocess.run('tasklist /FI "IMAGENAME eq terminal64.exe"',check=True,text=True,shell=True,capture_output=True)
+        _1=_.stdout.split()
+        _2=_1.index("Console")
+        try:
+            _3=_1.index("Services")
+            servicemanager.LogInfoMsg(str(_1[_3-1]))
+            _4=_=subprocess.run('taskkill /F /pid '+str(_1[_3-1]),check=True,text=True,shell=True,capture_output=True)
+            servicemanager.LogInfoMsg(str(_4))
+        except:
+            pass
+        return _2
+    except:
+        counter=0
+        return counter
+def checkservice():
+    _list=[None]*2
+    
+    while True:
+        _list.pop(0)
+        counter=_list[0]
+        _list.append(services())
+        servicemanager.LogInfoMsg(str(_list))
+        if services():
+            if _list[0]!=_list[1]:
+                mt5.initialize(login=login,password=password,server=server)
+                openpositions()
+        else:
+            if _list[0]!=_list[1]:
+                terminalconnect(login,password,server)
+                openpositions()
+        time.sleep(10)
+        services()
+    
 #creates the socket for communication
 def createsocket():
      global sock
@@ -69,14 +125,11 @@ def createsocket():
      sock.bind(("",2023))
      sock.listen()
      print("socket is listening")
-     
+    
      
      while True:
         t1=threading.Thread(target=createThread,args=(sock,))
         t1.start()
-        file=open("tradingreport.txt",mode='a+')
-        file.write("listening thread:" + str(t1.native_id) + '\n')
-        file.close()
         time.sleep(10)
         
         
@@ -99,10 +152,7 @@ def createThread(sock):
      servicemanager.LogInfoMsg(str(ATR)+pair+position)
      t2=threading.Thread(target=trade_manager,args=(pair,ATR,position,))
      t2.start()
-     file=open("tradingreport.txt",mode='a+')
-     file.write("listening thread:" + str(t2.native_id) + '\n')
-     file.close()
-     time.sleep(10)
+     time.sleep(1)
     except:
         servicemanager.LogErrorMsg(traceback.format_exc())
                      
@@ -151,33 +201,39 @@ def trade_manager(pair,ATR,position):
     
 #sets the initial limit
 def limits(dparsed,ATR,ticket,pair,position):
-     file=open("tradingreport.txt",mode='a+')
      if position=="true":
         lastPrice=dparsed
         while True:
            priceNow=mt5.symbol_info(pair).bid
+           tp=0.000
            if priceNow>=lastPrice:
               stoploss=priceNow-(ATR*1.5)
-              sl_request={"action":mt5.TRADE_ACTION_SLTP,"symbol":pair,"sl":stoploss,"tp":0.000,
+              sl_request={"action":mt5.TRADE_ACTION_SLTP,"symbol":pair,"sl":stoploss,"tp":tp,
         "position":ticket}
               sl=mt5.order_send(sl_request)
-              file.write(str(sl))
+              file=open('tradingreport.txt',mode='a+')
+              file.write('\n' + str(sl) + '\n')
               file.close()
               lastPrice=priceNow
+              time.sleep(60)
+            
               
               
      elif position=="false":
         lastPrice=dparsed
         while True:
            priceNow=mt5.symbol_info(pair).bid
+           tp=priceNow-ATR
            if priceNow<=lastPrice:
               stoploss=priceNow+(ATR*1.5)
-              sl_request={"action":mt5.TRADE_ACTION_SLTP,"symbol":pair,"sl":stoploss,"tp":0.000,
+              sl_request={"action":mt5.TRADE_ACTION_SLTP,"symbol":pair,"sl":stoploss,"tp":tp,
         "position":ticket}
               sl=mt5.order_send(sl_request)
               lastPrice=priceNow
-              file.write(str(sl))
+              file=open('tradingreport.txt',mode='a+')
+              file.write('\n' + str(sl) + '\n')
               file.close()
+              time.sleep(60)
               
              
      
